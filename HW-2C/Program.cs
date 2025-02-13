c9882b9f58b709b6d8d0b7da0d34daf48ff39211
Mouad Oulouali <mouadoune430@gmail.com>
2025-02-13 00:15:28 -0500

    Update Program.cs
    Added the menu to users

--- HW-2C/Program.cs
+++ HW-2C/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Menu
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**************");
+        Console.WriteLine("Menu: ");
+        Console.WriteLine("1 - New Game");
+        Console.WriteLine("2 - Load Game");
+        Console.WriteLine("3 - Options");
+        Console.WriteLine("4 - Quit");
+        Console.WriteLine("**************");
+    }
+}
